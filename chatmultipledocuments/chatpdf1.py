a8b6b5d98072eba469fb0d65ac17458cd65014b5
ANURAG SHARMA <146327546+anuragvisioner@users.noreply.github.com>
2024-09-29 02:53:21 +0530

    Update chatpdf1.py

--- chatmultipledocuments/chatpdf1.py
+++ chatmultipledocuments/chatpdf1.py
@@ -1,106 +1,134 @@
 import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-import os
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
+from langchain_google_genai import GoogleGenerativeAI
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain.schema import Document
+from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
+from langchain_core.prompts import MessagesPlaceholder, ChatPromptTemplate
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain.chains import create_retrieval_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
 
-load_dotenv()
-os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
-
-
-
-
-
+gemini_api_key = ""
+inference_api_key = ""
+# Define functions before they are called
 def get_pdf_text(pdf_docs):
-    text=""
+    documents = []
     for pdf in pdf_docs:
-        pdf_reader= PdfReader(pdf)
+        pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
-            text+= page.extract_text()
-    return  text
-
+            text = page.extract_text()  # Extract text from each page
+            if text:  # Check if text exists on the page
+                doc = Document(page_content=text)  # Convert text to Document object
+                documents.append(doc)
+    return documents  # Return a list of Document objects
 
 
 def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
-    chunks = text_splitter.split_text(text)
-    return chunks
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=200, length_function=len)
+    docs = text_splitter.split_documents(text)
+    return docs
 
 
-def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("faiss_index")
+def get_vectorstore(text_chunks):
+    embedding_model = HuggingFaceInferenceAPIEmbeddings(api_key=inference_api_key, model_name="sentence-transformers/all-MiniLM-l6-v2")
+    vectorstore = FAISS.from_documents(text_chunks, embedding_model)
+    return vectorstore
 
+def get_conversation_chain(vectorstore):
+    model = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=gemini_api_key)
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are a friendly AI Assistant that helps people with long documents. \n Note: ALWAYS START Your Answer Directly, without mentioning your name. Do Not add extra Spaces. Always write in third person.Always make your answer clear and complete. Answer only if a relevant question is asked, and if you know the answer, otherwise don't answer with unknown or vague information. Take care of the ethics. Answer the user's questions based on the relevant sentences: {context}"),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}")
+    ])
 
-def get_conversational_chain():
+    chain = create_stuff_documents_chain(
+        llm=model,
+        prompt=prompt
+    )
 
-    prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
-    Context:\n {context}?\n
-    Question: \n{question}\n
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
 
-    Answer:
-    """
+    retriever_prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+        ("user", "Given the above chat history, generate a search query to look up in order to get information relevant to the conversation")
+    ])
 
-    model = ChatGoogleGenerativeAI(model="gemini-pro",
-                             temperature=0.3)
+    history_aware_retriever = create_history_aware_retriever(
+        llm=model,
+        retriever=retriever,
+        prompt=retriever_prompt
+    )
 
-    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    retrieval_chain = create_retrieval_chain(
+        history_aware_retriever,
+        chain
+    )
 
-    return chain
+    return retrieval_chain
 
-
-
-def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    
-    new_db = FAISS.load_local("faiss_index", embeddings)
-    docs = new_db.similarity_search(user_question)
-
-    chain = get_conversational_chain()
-
-    
-    response = chain(
-        {"input_documents":docs, "question": user_question}
-        , return_only_outputs=True)
-
-    print(response)
-    st.write("Reply: ", response["output_text"])
-
-
-
+def process_chat(chain, question, chat_history):
+    response = chain.invoke({
+        "chat_history": chat_history,
+        "input": question,
+    })
+    return response.get("answer")
 
 def main():
-    st.set_page_config("Chat PDF")
-    st.header("Chat with PDF using Gemini💁")
+    load_dotenv()
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+    st.header("Chat with multiple PDFs :books:")
 
-    user_question = st.text_input("Ask a Question from the PDF Files")
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
 
-    if user_question:
-        user_input(user_question)
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("Send a message", key="user_input", max_chars = 2000):
+        if st.session_state.conversation is None:
+            st.error("Please upload PDFs to start the conversation")
+            return
+        st.chat_message("user").markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        response = process_chat(st.session_state.conversation, prompt, st.session_state.messages)
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
 
     with st.sidebar:
-        st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
-        if st.button("Submit & Process"):
-            with st.spinner("Processing..."):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                get_vector_store(text_chunks)
-                st.success("Done")
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # Get PDF text
+                pages = get_pdf_text(pdf_docs)
+
+                # Get the text chunks
+                chunks = get_text_chunks(pages)
+
+                # Create vector store
+                vectorstore = get_vectorstore(chunks)
+
+                # Create conversation chain
+                chain = get_conversation_chain(vectorstore)
+                st.session_state.conversation = chain
 
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
